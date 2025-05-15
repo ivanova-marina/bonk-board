7efac69f2a9b9474c077058293c9c1d457b5d13a
Marina Ivanova <marina0757@gmail.com>
2025-05-15 17:56:41 +0900

    set Name and HP

--- src/components/NpcCard/NpcCard.tsx
+++ src/components/NpcCard/NpcCard.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+
 export function NpcCard() {
+  const [npcName, setNpcName] = useState('');
+  const [npcHp, setNpcHp] = useState('');
   return (
     <>
       <label>
-        NPC name: <input name='npc-name' />
+        NPC name:
+        <input
+          name='npc-name'
+          value={npcName}
+          onChange={(e) => setNpcName(e.target.value)}
+        />
       </label>
       <label>
-        NPC hp: <input name='npc-hp' />
+        NPC hp:
+        <input
+          name='npc-hp'
+          value={npcHp}
+          onChange={(e) => setNpcHp(e.target.value)}
+        />
       </label>
       <div>
-        <p>MAX hp: </p>
-        <p>current hp: </p>
+        <p>Name: {npcName}</p>
+        <p>MAX hp: {npcHp}</p>
+        <p>current hp: {npcHp}</p>
       </div>
     </>
   );
