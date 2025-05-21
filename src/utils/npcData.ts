ff5ef8609acbb82c79a268b98d97a752490fbf6a
Marina Ivanova <marina0757@gmail.com>
2025-05-21 11:40:20 +0900

    use serialized Data

--- src/utils/npcData.ts
+++ src/utils/npcData.ts
@@ -1,9 +1,9 @@
 /** Save NPC's data to local storage */
 export function storeSavedNpc(name: string, hp: string) {
   const npcData = { name: name, hp: hp }
-  const storeSavedNpc = localStorage.setItem('npcData', JSON.stringify(npcData))
-
-  return storeSavedNpc
+  const serializedData = JSON.stringify(npcData);
+  localStorage.setItem('npcData', serializedData);
+  return serializedData
 }
 
 /** Get NPC's data from local storage */
